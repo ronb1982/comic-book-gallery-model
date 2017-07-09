5d6e595982c0c946cbc5a73bcee165ec541e46a4
Ron B <ronbnj@gmail.com>
2017-07-09 13:23:07 -0400

    Updated Program class to include LINQ queries and Eager related object loading

--- src/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ComicBookGalleryModel.Models;
 using System.Data.Entity;
+using System.Diagnostics;
 
 namespace ComicBookGalleryModel
 {
@@ -14,20 +15,66 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
-                // Get all comic books - include any relational objects
-                var comicBooks = context.ComicBooks
-                    .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists.Select(a => a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
+                // This line will output any SQL or database-related queries and operations
+                // to the Output window for debugging purposes.
+                context.Database.Log = (message) => Debug.WriteLine(message);
+
+                //var comicBooks = context.ComicBooks.ToList();
+
+                // Filter comic books by title where title contains the word "man"
+                /*var comicBooks = context.ComicBooks
+                                 .Include(cb => cb.Series)
+                                 .Where(cb => cb.Series.Title.Contains("man"))
+                                 .ToList();*/
+
+                // Sort by issue number descending
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .OrderByDescending(cb => cb.IssueNumber)
+                //    .ThenBy(cb => cb.PublishedOn)
+                //    .ToList();
+
+                var comicBooksQuery = context.ComicBooks
+                    .Include(cb => cb.Series) // Eager load Series entity
+                    .OrderByDescending(cb => cb.IssueNumber);
+
+                var comicBooks = comicBooksQuery.ToList();
+
+                var comicBooks2 = comicBooksQuery
+                    .Where(cb => cb.AverageRating < 7)
                     .ToList();
 
                 foreach (var comicBook in comicBooks)
                 {
-                    var artistRoleNames = comicBook.Artists.Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
-                    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
                     Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistRolesDisplayText);
                 }
+
+                Console.WriteLine();
+                Console.WriteLine("# of comic books: {0}", comicBooks.Count);
+                Console.WriteLine();
+
+                foreach (var comicBook in comicBooks2)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("# of comic books: {0}", comicBooks2.Count);
+
+                //// Get all comic books - include any relational objects
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    var artistRoleNames = comicBook.Artists.Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
 
                 Console.ReadLine();
             }
